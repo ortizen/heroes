ae4b2828204f6da0d895a633112f922959f46714
Enrique Ortiz <enrique.ortiz@outlook.com>
2020-02-10 20:44:13 -0600

    First commit

---
+++ lib/controller/heroes_controller.dart
@@ -0,0 +1,26 @@
+import 'package:aqueduct/aqueduct.dart';
+import 'package:heroes/heroes.dart';
+
+class HeroesController extends Controller {
+  final _heroes = [
+    {'id': 11, 'name': 'Mr. Nice'},
+    {'id': 12, 'name': 'Narco'},
+    {'id': 13, 'name': 'Bombasto'},
+    {'id': 14, 'name': 'Celeritas'},
+    {'id': 15, 'name': 'Magneta'},
+  ];
+
+  @override
+  Future<RequestOrResponse> handle(Request request) async {
+    if (request.path.variables.containsKey('id')) {
+      final id = int.parse(request.path.variables['id']);
+      final hero =
+          _heroes.firstWhere((hero) => hero['id'] == id, orElse: () => null);
+      if (hero == null) {
+        return Response.notFound();
+      }
+      return Response.ok(hero);
+    }
+    return Response.ok(_heroes);
+  }
+}
